6c15d5674609dc89dd6e5fff19fa3f25e903e5cf
kairosity <karinafinegan@gmail.com>
2021-03-30 11:48:06 +0000

    change db var name to match heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DB_URI'))
+    'default': dj_database_url.parse(os.environ.get(DATABASE_URL))
 }
 
 
